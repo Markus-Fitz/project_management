249733359e01ad3d9cb3dc2bbb0c2bb6d96270a8
Markus Fitz <markus.fitz.2@gmail.com>
2026-05-29 12:32:54 +0200

    Removed tests for creating files individually in favor of creation with copy_folder_template function. Added tests.

--- main.py
+++ main.py
@@ -21,35 +21,26 @@ PROJECT_RESEARCH_NOTE_DIR = PROJECT_DIR / "Research_notes"
 DEVELOPMENT_TEMPLATE_PATH = PARENT_DIR / "project_templates" / "Template_development"
 RESEARCH_TEMPLATE_PATH = PARENT_DIR / "project_templates" / "Template_research"
 
-# creating Task directory and new Task.md from template
-copy_file_template(DEVELOPMENT_TEMPLATE_PATH / "Templates" / "Task.md", PROJECT_TASK_DIR / "test_task.md")
-update_frontmatter(PROJECT_TASK_DIR / "test_task.md", {"status": "active", "id": "TASK-000"})
+# Create a folder from a template directory
+try:
+    copy_folder_template("test_project_from_template_1")
+except FileExistsError:
+    print("A project with that name already exists.")
+except FileNotFoundError:
+    print("The template could not be found.")
 
-# creating Purchase directory and new Purchase.md from template
-copy_file_template(DEVELOPMENT_TEMPLATE_PATH / "Templates" / "Purchase.md", PROJECT_PURCHASE_DIR / "test_purchase.md")
-update_frontmatter(PROJECT_PURCHASE_DIR / "test_purchase.md", {"amount": 100, "id": "PUR-000"})
+# File exists error should be thrown
+try:
+    copy_folder_template("test_project_from_template_1")
+except FileExistsError:
+    print("A project with that name already exists.")
+except FileNotFoundError:
+    print("The template could not be found.")
 
-# creating Supplier directory and new Supplier.md from template
-copy_file_template(DEVELOPMENT_TEMPLATE_PATH / "Templates" / "Supplier.md", PROJECT_SUPPLIER_DIR / "test_supplier.md")
-update_frontmatter(PROJECT_SUPPLIER_DIR / "test_supplier.md", {"name": "Amazon", "id": "SUP-000"})
-
-# creating Research_note directory and new Research_note.md from template
-copy_file_template(DEVELOPMENT_TEMPLATE_PATH / "Templates" / "Research_note.md", PROJECT_RESEARCH_NOTE_DIR / "test_research_note.md")
-update_frontmatter(PROJECT_RESEARCH_NOTE_DIR / "test_research_note.md", {"source": "www.test_source.org", "id": "RES-000"})
-
-meta, body = read_file(PROJECT_TASK_DIR / "test_task.md")
-print("Updated Task meta: \n")
-print(meta)  # should show updated fields
-
-meta, body = read_file(PROJECT_PURCHASE_DIR / "test_purchase.md")
-print("Updated Purchase meta: \n")
-print(meta)  # should show updated fields
-
-meta, body = read_file(PROJECT_SUPPLIER_DIR / "test_supplier.md")
-print("Updated Supplier meta: \n")
-print(meta)  # should show updated fields
-
-meta, body = read_file(PROJECT_RESEARCH_NOTE_DIR / "test_research_note.md")
-print("Updated Research-note meta: \n")
-print(meta)  # should show updated fields
-
+# The template could not be found error should be thrown
+try:
+    copy_folder_template("test_project_from_template_2", PARENT_DIR / "non-existant-folder")
+except FileExistsError:
+    print("A project with that name already exists.")
+except FileNotFoundError:
+    print("The template could not be found.")
