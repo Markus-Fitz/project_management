74db78e3596dc5fca549c577720191bbf3691aa8
Markus Fitz <markus.fitz.2@gmail.com>
2026-06-03 10:36:51 +0200

    Modified add_task for MCP integration: Path -> str, returns str.

--- pm_src/main.py
+++ pm_src/main.py
@@ -50,8 +50,8 @@ RESEARCH_TEMPLATE_PATH = PARENT_DIR / "project_templates" / "Template_research"
 initialize_project("test_dev-proj", "Template_development")
 initialize_project("test_res-proj", "Template_research")
 
-add_task(PARENT_DIR / "test_dev-proj", "test_task_ID-should-be-001")
-add_task(PARENT_DIR / "test_dev-proj", "test_task_ID-should-be-002")
+add_task("test_dev-proj", "test_task_ID-should-be-001")
+add_task("test_dev-proj", "test_task_ID-should-be-002")
 
 add_research_note(PARENT_DIR / "test_dev-proj", "test_note_ID-should-be-001")
 add_research_note(PARENT_DIR / "test_dev-proj", "test_note_ID-should-be-002")
